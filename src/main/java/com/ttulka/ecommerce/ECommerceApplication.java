f49cd9eba52cccb5c3397f439990b8ff0ac89bee
Ritesh Kumar <ritesh.kumar@locuz.com>
2025-04-30 18:36:16 +0530

    Modification done to deploy in stand alone Tomcat server

--- src/main/java/com/ttulka/ecommerce/ECommerceApplication.java
+++ src/main/java/com/ttulka/ecommerce/ECommerceApplication.java
@@ -3,16 +3,25 @@ package com.ttulka.ecommerce;
 import com.ttulka.ecommerce.common.events.EventPublisher;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 /**
  * Spring Boot based monolithic application.
  */
 @SpringBootApplication
+@ComponentScan(basePackages = "com.ttulka.ecommerce")
 @EnableAsync
-public class ECommerceApplication {
+public class ECommerceApplication extends SpringBootServletInitializer {
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ECommerceApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ECommerceApplication.class, args);
